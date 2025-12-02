88324e833f6ecb55ffe45b1401970c94ef68c2d4
Linus Schneeberg <schlafhase190@gmail.com>
2025-12-02 17:36:32 +0100

    Initial commit

---
+++ Polynomial.cs
@@ -0,0 +1,142 @@
+using System.Numerics;
+using System.Text;
+
+namespace Polynomials;
+
+public class Polynomial
+{
+    public int Degree { get; init; }
+    public List<Complex> Coefficients { get; private init; }
+
+    public Polynomial(int degree, List<Complex> coefficients)
+    {
+        Degree = degree;
+        Coefficients = coefficients.Count == degree + 1 ?
+          coefficients :
+          throw new ArgumentException("Polynomial must have degree + 1 coefficients");
+
+        if (Coefficients[Degree] == 0)
+        {
+            throw new ArgumentException("nth coefficient must not be 0");
+        }
+    }
+
+    public Complex Evaluate(Complex x)
+    {
+        return Coefficients
+          .AsEnumerable()
+          .Reverse()
+          .Aggregate((result, coeff) => result * x + coeff);
+    }
+
+    public Polynomial Derivative =>
+      new Polynomial(Degree - 1, Coefficients.Slice(1, Coefficients.Count - 1).Select((c, i) => c * (i + 1)).ToList());
+
+    public (double upper, double lower) Bounds
+    {
+        get
+        {
+            double upper = 1 + Coefficients.Select(c => (c / Coefficients[Coefficients.Count - 1]).Magnitude).Max();
+            double lower = 1 / upper;
+            return (upper, lower);
+        }
+    }
+
+    private List<Complex> fibonacciAnnulus(int n, double rMin, double rMax)
+    {
+        double goldenRatio = (1d + Math.Sqrt(5d)) / 2d;
+        return (from i in Enumerable.Range(0, n)
+                let t = (double)i / n
+                let r = Math.Sqrt(t * (rMax * rMax - rMin * rMin) + rMin * rMin)
+                let theta = 2d * Math.PI * i / goldenRatio
+                select new Complex(r * Math.Cos(theta), r * Math.Sin(theta)))
+          .ToList();
+    }
+
+    public List<Complex> GetRootsAberth(int maxIterations = 100000, double threshold = 0.0000001)
+    {
+        (double upper, double lower) bounds = Bounds;
+        List<Complex> roots = fibonacciAnnulus(Degree, bounds.lower, bounds.upper);
+        Polynomial derivative = Derivative;
+
+        for (int i = 0; i < maxIterations; i++)
+        {
+            List<Complex> offsets = new List<Complex>(Degree);
+
+            for (int k = 0; k < Degree; k++)
+            {
+                Complex zk = roots[k];
+                Complex thisOverDerivative = (Evaluate(zk) / derivative.Evaluate(zk));
+
+                Complex sum = Complex.Zero;
+                for (int j = 0; j < Degree; j++)
+                {
+                    if (j != k)
+                    {
+                        sum += 1d / (zk - roots[j]);
+                    }
+                }
+
+                offsets.Add(thisOverDerivative / (1 - thisOverDerivative * sum));
+            }
+
+            for (int k = 0; k < Degree; k++)
+            {
+                roots[k] -= offsets[k];
+            }
+
+            if (offsets.Select(o => o.Magnitude).Max() < threshold)
+            {
+                return roots;
+            }
+        }
+
+        return roots;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new();
+        for (int i = 0; i < Coefficients.Count; i++)
+        {
+            Complex c = Coefficients[i];
+            if (c != 0)
+            {
+                sb.Insert(0, "(" + c.ToString() + ")x^" + i + "+ ");
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    public static List<Polynomial> LittlewoodUpTo(int n)
+    {
+        List<Polynomial> result = [];
+        for (int i = 1; i < n; i++)
+        {
+            result.AddRange(Littlewood(i));
+        }
+
+        return result;
+    }
+
+    public static IEnumerable<Polynomial> Littlewood(int n)
+    {
+        List<Complex> choices = [new Complex(-1, 0), new Complex(1, 0)];
+
+        return cartesian(Enumerable.Repeat(choices, n + 1))
+          .Select(coeffs => new Polynomial(n, coeffs.ToList()));
+    }
+
+    private static IEnumerable<IEnumerable<T>> cartesian<T>(IEnumerable<IEnumerable<T>> sequences)
+    {
+        IEnumerable<IEnumerable<T>> emptyProduct = new[] { Enumerable.Empty<T>() };
+
+        return sequences.Aggregate(
+            emptyProduct,
+            (acc, seq) =>
+              from set in acc
+              from item in seq
+              select set.Concat(new[] { item }));
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Numerics;
+using ScottPlot;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.ColorSpaces;
+using SixLabors.ImageSharp.ColorSpaces.Conversion;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Drawing;
+
+namespace Polynomials;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        createColouredImage(15, 2);
+        return;
+        List<Polynomial> littlewoods = Polynomial.LittlewoodUpTo(15);
+        List<Complex> roots = [];
+
+        foreach (var p in littlewoods)
+        {
+            roots.AddRange(p.GetRootsAberth());
+        }
+
+
+        Plot plot = new();
+
+
+        plot.Add.Markers(roots.Select(r => r.Real).ToArray(), roots.Select(r => r.Imaginary).ToArray(), MarkerShape.FilledCircle, 2);
+        plot.SavePng("plot.png", 1920, 1080);
+    }
+
+    private static void createColouredImage(int n, double scale)
+    {
+        SixLabors.ImageSharp.Image img = new SixLabors.ImageSharp.Image<Rgba32>(1920, 1080);
+        img.Mutate(x => x.Fill(SixLabors.ImageSharp.Color.Black));
+
+        goto roots;
+        for (int i = 1; i < n; i++)
+        {
+            List<Polynomial> littlewoods = Polynomial.Littlewood(i).ToList();
+            Console.WriteLine($"Solving Polynomials of degree {i}");
+            foreach (Polynomial l in littlewoods)
+            {
+                img.Mutate(ctx => ctx.ProcessPixelRowsAsVector4((span, point) =>
+                {
+                    for (int x = 0; x < span.Length; x++)
+                    {
+                        // Calculate the actual x coordinate
+                        int actualX = point.X + x;
+                        int y = point.Y;
+
+                        // Modify the pixel based on coordinates
+                        // span[x] contains the RGBA values as Vector4 (values from 0.0 to 1.0)
+                        var pixel = span[x];
+                        var uvX = ((double)actualX / img.Height - 0.5 * img.Width / img.Height) * 2;
+                        var uvY = -((double)y / img.Height - 0.5) * 2;
+                        Complex c = new Complex(uvX * scale, uvY * scale);
+                        Complex r = l.Evaluate(c);
+                        double a = Math.Atan2(r.Imaginary, r.Real);
+                        var col = new Hsl((float)(a / Math.PI * 180), 1, (float)((0.1 / Math.Exp(r.Magnitude) + 0.1)));
+
+                        var rgb = ColorSpaceConverter.ToRgb(col);
+
+                        pixel.X += rgb.R / n;
+                        pixel.Y += rgb.G / n;
+                        pixel.Z += rgb.B / n;
+                        // pixel.X = rgb.R;
+                        // pixel.Y = rgb.G;
+                        // pixel.Z = rgb.B;
+                        pixel.W = 1.0f;
+
+                        span[x] = pixel;
+                    }
+                }));
+
+            }
+        }
+
+    roots:
+
+        for (int i = 1; i < n; i++)
+        {
+            Console.WriteLine($"Finding roots {i}");
+            List<Complex> roots = [];
+            object rootsLock = new object();
+
+            Parallel.ForEach(Polynomial.Littlewood(i), l =>
+            {
+                var r = l.GetRootsAberth();
+                lock (rootsLock)
+                {
+                    roots.AddRange(r);
+                }
+            });
+
+            Hsl col = new Hsl((float)((double)i / n * 360), 1, 0.5f);
+            var rgb = ColorSpaceConverter.ToRgb(col);
+
+            foreach (Complex root in roots)
+            {
+                EllipsePolygon circle = new(new PointF((float)(root.Real / scale / 2 + 0.5 * img.Width / img.Height) * img.Height, (float)(-root.Imaginary / scale / 2 + 0.5) * img.Height), 1f);
+                img.Mutate(x => x.Fill(new SixLabors.ImageSharp.Color(new Vector4(rgb.R, rgb.G, rgb.B, 2f / n)), circle));
+            }
+        }
+        img.Save("out.png");
+    }
+}
