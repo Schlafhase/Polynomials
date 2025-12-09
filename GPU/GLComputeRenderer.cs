a1e663b8f63451de7ba214bfed1a4bc95d8aad4b
Linus Schneeberg <schlafhase190@gmail.com>
2025-12-09 08:28:40 +0100

    remove compute header from shader for compatibility

--- GPU/GLComputeRenderer.cs
+++ GPU/GLComputeRenderer.cs
@@ -41,7 +41,6 @@ public class GLComputeRenderer : IDisposable
 
         const string computeShaderSource =
             @"
-#[compute]
 #version 430 core
 layout(local_size_x = 16, local_size_y = 16) in;
 
